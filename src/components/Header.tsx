c8b3e3346aa27e8d83bc2cfbeda50fb4c5e26f44
Cathal Mac Donnacha <macdonnacha.cathal@gmail.com>
2020-06-15 19:14:18 +0100

    Updated Header styling 🤳

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { screenSize } from 'constants/screenSizes';
+import redditLogo from 'assets/reddit-logo.png';
+import redditIcon from 'assets/reddit-icon.png';
+import SearchBar from './SearchBar';
+import userProfileImage from 'assets/user-profile.png';
+import Avatar from './Avatar';
 
 const Container = styled.header`
   grid-area: header;
@@ -11,10 +17,43 @@ const Container = styled.header`
   padding: 0 20px;
 `;
 
+const RedditLogo = styled.img`
+  height: 30px;
+
+  @media (max-width: ${screenSize.medium}) {
+    display: none;
+  }
+`;
+
+const RedditIcon = styled.img`
+  height: 30px;
+
+  @media (min-width: ${screenSize.medium}) {
+    display: none;
+  }
+`;
+
+const Username = styled.span`
+  margin-right: 15px;
+
+  @media (max-width: ${screenSize.medium}) {
+    display: none;
+  }
+`;
+
 const Header = () => {
   return (
     <Container>
-      <h1>Header</h1>
+      <RedditLogo src={redditLogo} />
+      <RedditIcon src={redditIcon} />
+      <SearchBar
+        placeholder={'Type a subreddit name'}
+        onSearchTextChanged={() => {
+          return 'hi';
+        }}
+      />
+      <Username>Cathal Mac Donnacha</Username>
+      <Avatar src={userProfileImage} />
     </Container>
   );
 };
