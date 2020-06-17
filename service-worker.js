5fbb0dca5fd983a73ce7112b28e531bbccb21b78
cmacdonnacha <macdonnacha.cathal@gmail.com>
2020-06-17 22:37:05 +0000

    Deploying to gh-pages from  @ 6ce87b29839c894f578c22849d9ce66457cfb732 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-reddit-search/precache-manifest.a5a77072afeed63c37925adc9ee99892.js"
+  "/react-reddit-search/precache-manifest.edd230d1621bb3032a06781d7a41629d.js"
 );
 
 self.addEventListener('message', (event) => {
