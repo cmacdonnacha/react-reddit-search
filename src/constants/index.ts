d61929ba41a7f406052fde8a7448078948379791
cmacdonnacha <cathal.macdonnacha@wpengine.com>
2022-01-24 16:24:48 +0000

    Deploying to master from  @ 0460dd19f3d40e8034dafca2d20b137f3a3edad3 🚀

--- src/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export const REDDIT_URL = 'https://www.reddit.com';
-export const POSTS_LIMIT = 10;
-export const DEFAULT_SUBREDDIT = 'pics';
--- src/constants/screenSizes.ts
+++
@@ -1,6 +0,0 @@
-export const screenSize = {
-  small: '320px',
-  medium: '640px',
-  large: '1080px',
-  extraLarge: '1200px',
-};
